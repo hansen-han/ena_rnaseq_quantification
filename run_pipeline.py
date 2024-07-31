57be36f03e2599bb97e66c5f994dc1c3d3468294
Hansen Han <hansen.h97@gmail.com>
2024-07-30 22:15:19 -0400

    Add handling to run multiple projects at once

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,13 +2,19 @@ import sys
 from ena_pipeline import download_and_quantify_bioproject_fastqs
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python run_pipeline.py <BioProject ID>")
+    if len(sys.argv) < 2:
+        print("Usage: python run_pipeline.py <BioProject IDs>")
         sys.exit(1)
-
-    project_id = sys.argv[1]
-    print(f"Running Pipeline for Project: {project_id}")
-    download_and_quantify_bioproject_fastqs(project_id)
+    
+    # run for each argument
+    project_ids = sys.argv[1:]
+    for project_id in project_ids:
+        try:
+            print(f"Running Pipeline for Project: {project_id}")
+            download_and_quantify_bioproject_fastqs(project_id)
+        except Exception as e:
+            print("Failure running project:", project_id)
+            print("Error:", e)
 
 if __name__ == "__main__":
     main()
